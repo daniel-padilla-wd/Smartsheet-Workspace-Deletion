9545eda14902ffa1ac949183ff93e26e8e3639ed
Daniel Padilla <daniel.padilla@workday.com>
2025-12-02 02:27:08 -0800

    adding keys

--- app/app_aws_oauth.py
+++ app/app_aws_oauth.py
@@ -10,10 +10,10 @@ from botocore.exceptions import ClientError
 
 # --- Configuration ---
 # AWS Secrets Manager secret names
-ACCESS_TOKEN_SECRET = "accessToken"
-REFRESH_TOKEN_SECRET = "refreshToken"
-CLIENT_ID_SECRET = "CLIENT_ID"
-CLIENT_SECRET_SECRET = "CLIENT_SECRET"
+ACCESS_TOKEN_SECRET = "ausw2p-smgr-smt-access-token-001"
+REFRESH_TOKEN_SECRET = "ausw2p-smgr-smt-refresh-token-002"
+CLIENT_ID_SECRET = "ausw2p-smgr-smt-client-id-003"
+CLIENT_SECRET_SECRET = "ausw2p-smgr-smt-client-secret-004"
 
 # Smartsheet OAuth endpoint for token refresh
 TOKEN_URL = "https://api.smartsheet.com/2.0/token"
@@ -24,7 +24,7 @@ def get_secret_string(secret_name):
     client = boto3.client('secretsmanager')
     try:
         resp = client.get_secret_value(SecretId=secret_name)
-
+        
         if 'SecretString' in resp:
             return resp['SecretString']
         else:
@@ -40,8 +40,8 @@ def get_secret_string(secret_name):
 
 def get_oauth_credentials():
     """Retrieve OAuth client ID and secret from separate AWS Secrets Manager secrets."""
-    client_id = get_secret_string(CLIENT_ID_SECRET)
-    client_secret = get_secret_string(CLIENT_SECRET_SECRET)
+    client_id = get_secret_string(CLIENT_ID_SECRET)["CLIENT_ID"]
+    client_secret = get_secret_string(CLIENT_SECRET_SECRET)["CLIENT_SECRET"]
     
     if client_id and client_secret:
         return client_id, client_secret
@@ -173,8 +173,8 @@ def get_smartsheet_client(scopes=None):
         smartsheet.Smartsheet: Authenticated Smartsheet client or None if authentication fails
     """
     # Get tokens from separate Secrets Manager secrets
-    access = get_secret_string(ACCESS_TOKEN_SECRET)
-    refresh = get_secret_string(REFRESH_TOKEN_SECRET)
+    access = get_secret_string(ACCESS_TOKEN_SECRET)["accessToken"]
+    refresh = get_secret_string(REFRESH_TOKEN_SECRET)["refreshToken"]
 
     # If we have an access token, create a client
     if access:
